62a01239b96f60dbfb695ca4768eb75478e66808
SebastiánEPH <sebastianeph99@gmail.com>
2020-04-06 15:01:51 -0500

    add SendGmail

---
+++ SendGmail.py
@@ -0,0 +1,30 @@
+# Importamos librería, enviar Gmail
+from email.mime.multipart import MIMEMultipart;
+from email.mime.text import MIMEText;
+import smtplib;
+import datetime;
+
+
+def SendGmail(emailUser,emailPass,emailTo,message):
+    txt = MIMEMultipart();
+    # Enviar Desde
+    txt["From"] = emailUser;
+    # a
+    txt["To"] = emailTo;
+    txt["Subject"] = "Report"+  str(datetime.datetime.now().date());
+    txt.attach(MIMEText(message,"plain"));
+    try:
+        # Conexion al server de Google
+        server = smtplib.SMTP('smtp.gmail.com: 587');
+        # Inicia Conexion
+        server.starttls();
+        # Inicia sesión [User : emailPass] = google.com
+        server.login(txt["From"], emailPass);
+        # Envía Mensaje 
+        server.sendmail(txt["From"], txt["To"], txt.as_string());
+        # Cierra Conexión al servidor
+        server.quit();
+        # En caso de Envio Exitoso , retorna un True
+        return True;
+    except:
+        return False;
